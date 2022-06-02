acd9e8f7c0cabe41b57f849f41e943874b3117f1
krete77 <ryanoconnor.dev@gmail.com>
2022-06-02 09:26:17 -0400

    added plotting data

---
+++ populations.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = np.loadtxt('populations.txt')
+year, hares, lynxes, carrots = data.T    #  trick: column to variables
+
+plt.axes([0.2, 0.1, 0.5, 0.8])
+plt.plot(year, hares, year, lynxes, year, carrots)
+plt.legend(('Hare', 'Lynx', 'Carrot'), loc=(1.05, 0.5))
+
